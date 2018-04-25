de9ecdd6c96815a76fd576a8579c5e0bac9ae0ab
徐琳 <xulin@xulindeMacBook-Pro.local>
2018-04-25 08:08:11 +0800

    develop vote

--- tgcnbot/vote/handlers.py
+++ tgcnbot/vote/handlers.py
@@ -3,25 +3,41 @@ from telegram import (InlineKeyboardButton, InlineKeyboardMarkup,
                       ReplyKeyboardMarkup, KeyboardButton)
 from telegram.ext import (Filters, CommandHandler, CallbackQueryHandler,
                           MessageHandler, ConversationHandler)
-
+from tgcnbot.vote.models import Vote, Joiner
+from tgcnbot.user.models import save_user
 
 def report(bot, update):
     print(update.message)
-    if not update.message.reply_to_message:
+    reply_to_message = update.message.reply_to_message
+    if not reply_to_message:
         # Todo
         # 删除信息，回复提醒需要使用 report 命令回复举报信息；15秒后自动删除
         return
+    #Todo 把这里整理出方法
+    chat_id = reply_to_message.chat.id
+    message_id = reply_to_message.message_id
+    text = reply_to_message.text
+    vote = Vote.query.filter(
+        Vote.chat_id == chat_id,
+        Vote.message_id == message_id).first()
+    if vote:
+        return
+    vote = Vote(
+        chat_id=chat_id,
+        message_id=message_id,
+        text=text)
+    vote.save()
     content = "该消息被举报，下面进入表决。"
     buttons = [
         [
             InlineKeyboardButton(
-                'Spam 消息',
+                'Spam 消息 0',
                 callback_data='report:spam'),
             InlineKeyboardButton(
-                '违反群规',
+                '违反群规 0',
                 callback_data='report:break'),
             InlineKeyboardButton(
-                '取消表决',
+                '取消表决 0',
                 callback_data='report:cancel')],
     ]
     bot.sendMessage(
@@ -32,20 +48,67 @@ def report(bot, update):
     update.message.delete()
 
 
-def process_report(bot, update):
+def result(bot, job):
+    vote = Vote.query.get(*job.context)
+    content = \
+    """
+    投票结果为：\n
+    1. Spam 消息 {}票\n
+    2. 违反群规 {}票\n
+    3. 取消表决 {}票\n
+    """.format(
+        len(vote.spam_tickets),
+        len(vote.break_tickets),
+        len(vote.cancel_tickets))
+    bot.editMessageText(
+        chat_id=vote.chat_id,
+        
+    )
+
+def vote(bot, update):
     print(update.callback_query)
+    reply_to_message = update.callback_query.message.reply_to_message
     callback_data = update.callback_query.data
+    chat_id = reply_to_message.chat.id
+    message_id = reply_to_message.message_id
+    vote = Vote.query.filter(
+        Vote.chat_id == chat_id,
+        Vote.message_id == message_id).first()
+    if not vote:
+        return
+    user = save_user(update.callback_query.from_user)
     report_type = next(iter(re.findall(r":([a-z]+)", callback_data)), None)
+
+
+    joiner = Joiner.query.filter(
+        Joiner.user == user,
+        Joiner.vote == vote).first()
+    if not joiner:
+        joiner = Joiner(user=user, vote=vote, ticket=report_type)
+        joiner.save()
+    elif joiner.ticket == report_type:
+        joiner.delete()
+    else:
+        joiner.ticket = report_type
+        joiner.save()
+
+    vote = Vote.query.filter(
+        Vote.chat_id == chat_id,
+        Vote.message_id == message_id).first()
+
+    for joiner in vote.joiners:
+        print(joiner.vote_id, joiner.user_id, joiner.ticket)
+    
     buttons=[
         [
             InlineKeyboardButton(
-                'Spam 消息',
+                'Spam 消息 {}'.format(len(vote.spam_tickets)),
                 callback_data='report:spam'),
             InlineKeyboardButton(
-                '违反群规',
+                '违反群规 {}'.format(len(vote.break_tickets)),
                 callback_data='report:break'),
             InlineKeyboardButton(
-                '取消表决',
+                '取消表决 {}'.format(len(vote.cancel_tickets)),
                 callback_data='report:cancel')],
     ]
     update.callback_query.edit_message_text(
@@ -57,5 +120,5 @@ def process_report(bot, update):
 
 handlers = [
     CommandHandler('report', report),
-    CallbackQueryHandler(process_report, pattern='report:')
+    CallbackQueryHandler(vote, pattern='report:')
 ]
--- tgcnbot/vote/models.py
+++ tgcnbot/vote/models.py
@@ -1,14 +1,71 @@
 from datetime import datetime
+from sqlalchemy.util import hybridproperty
 from tgcnbot.extensions import db
 from tgcnbot.utils.database import CRUDMixin
 
+def save_vote(data):
+    vote = Vote.query.filter(
+        Vote.chat_id == data.chat.id,
+        Vote.message_id == data.message_id).first()
+    if not vote:
+        vote = Vote(
+            chat_id=data.chat.id,
+            message_id=data.message_id,
+            text=data.text)
+        vote.save()
+    return vote
 
-class Vote(db.Model):
+class Joiner(db.Model, CRUDMixin):
+    __tablename__ = 'joiner'
+    vote_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, primary_key=True)
+    ticket = db.Column(db.String(50))
+    user = db.relationship(
+        'User',
+        primaryjoin="Joiner.user_id==User.id",
+        foreign_keys='Joiner.user_id')
+
+
+class Vote(db.Model, CRUDMixin):
     id = db.Column(db.Integer, primary_key=True)
     chat_id = db.Column(db.Integer)
+    message_id = db.Column(db.Integer)
+    # Todo target_id and message_id
+    text = db.Column(db.Text)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
     chat = db.relationship(
         'Chat',
         primaryjoin="Chat.id == Vote.chat_id",
         foreign_keys='Vote.chat_id',
-        back_populates='votes')
+        backref='votes')
+    
+    users = db.relationship(
+        'User',
+        # secondary="join(User, Joiner.user_id == User.id)",
+        secondary='joiner',
+        primaryjoin='Vote.id == foreign(Joiner.vote_id)',
+        secondaryjoin='foreign(Joiner.user_id) == User.id',
+        foreign_keys='[Joiner.vote_id, Joiner.user_id]',
+        backref='votes')
+    
+    joiners = db.relationship(
+        'Joiner',
+        primaryjoin='Vote.id == Joiner.vote_id',
+        foreign_keys='Joiner.vote_id',
+        backref="vote")
+
+    spam_tickets = db.relationship(
+        'Joiner',
+        primaryjoin='and_(Vote.id == Joiner.vote_id, Joiner.ticket == "spam")',
+        foreign_keys='Joiner.vote_id')
+   
+    break_tickets = db.relationship(
+        'Joiner',
+        primaryjoin='and_(Vote.id == Joiner.vote_id, Joiner.ticket == "break")',
+        foreign_keys='Joiner.vote_id')
+
+    cancel_tickets = db.relationship(
+        'Joiner',
+        primaryjoin='and_(Vote.id == Joiner.vote_id, Joiner.ticket == "cancel")',
+        foreign_keys='Joiner.vote_id')
